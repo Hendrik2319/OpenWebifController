a12cd3d603300db544077b77d36e126c0b189a89
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-03-21 13:19:18 +0000

    JAVA: [Eclipse] - Projekt "OpenWebifController"    - angelegt
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/OpenWebifController@5540 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

---
+++ src/net/schwarzbaer/java/tools/openwebifcontroller/OpenWebifController.java
@@ -0,0 +1,10 @@
+package net.schwarzbaer.java.tools.openwebifcontroller;
+
+public class OpenWebifController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

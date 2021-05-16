7050421653e67f678667219429936e218569a4b9
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-05-16 01:40:49 +0000

    JAVA: [Eclipse] - Projekt "OpenWebifController"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/OpenWebifController@5680 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/tools/openwebifcontroller/bouquetsnstations/EPGDialog.java
+++ src/net/schwarzbaer/java/tools/openwebifcontroller/bouquetsnstations/EPGDialog.java
@@ -4,12 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.Window;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.MouseWheelEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.HashMap;
@@ -24,8 +28,6 @@ import javax.swing.JSlider;
 
 import net.schwarzbaer.gui.Canvas;
 import net.schwarzbaer.gui.StandardDialog;
-import net.schwarzbaer.gui.Tables;
-import net.schwarzbaer.gui.Tables.LabelRendererComponent;
 import net.schwarzbaer.java.lib.openwebif.Bouquet;
 import net.schwarzbaer.java.lib.openwebif.EPG;
 import net.schwarzbaer.java.lib.openwebif.EPGevent;
@@ -63,6 +65,38 @@ public class EPGDialog extends StandardDialog {
 		
 		JScrollBar epgViewHorizScrollBar = new JScrollBar(JScrollBar.HORIZONTAL);
 		JScrollBar epgViewVertScrollBar  = new JScrollBar(JScrollBar.VERTICAL);
+		//System.out.printf("BlockIncrement: %d%n", epgViewVertScrollBar.getBlockIncrement());
+		//System.out.printf("UnitIncrement : %d%n", epgViewVertScrollBar.getUnitIncrement());
+		
+		epgViewVertScrollBar.addAdjustmentListener(e -> {
+			epgView.setRowOffsetY(e.getValue());
+		});
+		epgViewVertScrollBar.setValues(epgView.getRowOffsetY(), epgView.getRowViewHeight(), 0, epgView.getContentHeight());
+		
+		epgView.addMouseWheelListener(e -> {
+			JScrollBar scrollBar = epgViewVertScrollBar;
+			if (epgView.getRowViewHeight()>=epgView.getContentHeight())
+				return;
+			
+			int scrollType = e.getScrollType();
+			//String scrollTypeStr = "???";
+			int increment = 0;
+			switch (scrollType) {
+			case MouseWheelEvent.WHEEL_UNIT_SCROLL : /*scrollTypeStr = "UNIT" ;*/ increment = scrollBar.getUnitIncrement();  break;
+			case MouseWheelEvent.WHEEL_BLOCK_SCROLL: /*scrollTypeStr = "BLOCK";*/ increment = scrollBar.getBlockIncrement(); break;
+			}
+			int wheelRotation = e.getWheelRotation();
+			//System.out.printf("ScrollAmount(\"%s\"): %d x %d -> %d%n", scrollTypeStr, wheelRotation, increment, wheelRotation*increment);
+			
+			int value   = scrollBar.getValue();
+			int visible = scrollBar.getVisibleAmount();
+			int maximum = scrollBar.getMaximum();
+			
+			value += wheelRotation*increment;
+			if      (value>=0 && value+visible<=maximum) scrollBar.setValue(value);
+			else if (value<0)                            scrollBar.setValue(0);
+			else if (value+visible>maximum)              scrollBar.setValue(maximum-visible);
+		});
 		
 		JButton closeButton = OpenWebifController.createButton("Close", true, e->closeDialog());
 		
@@ -94,7 +128,26 @@ public class EPGDialog extends StandardDialog {
 		addComponentListener(new ComponentListener() {
 			@Override public void componentShown  (ComponentEvent e) {}
 			@Override public void componentHidden (ComponentEvent e) {}
-			@Override public void componentResized(ComponentEvent e) { OpenWebifController.settings.putDimension(ValueKey.EPGDialogWidth,ValueKey.EPGDialogHeight,EPGDialog.this.getSize()); }
+			@Override public void componentResized(ComponentEvent e) {
+				OpenWebifController.settings.putDimension(ValueKey.EPGDialogWidth,ValueKey.EPGDialogHeight,EPGDialog.this.getSize());
+				int rowOffsetY = epgView.getRowOffsetY();
+				int viewHeight = epgView.getRowViewHeight();
+				int contentHeight = epgView.getContentHeight();
+				if (rowOffsetY+viewHeight>contentHeight) {
+					rowOffsetY = contentHeight-viewHeight;
+					if (rowOffsetY<0) {
+						rowOffsetY = 0;
+						viewHeight = contentHeight;
+						epgViewVertScrollBar.setEnabled(false);
+					} else
+						epgViewVertScrollBar.setEnabled(true);
+					epgView.setRowOffsetY(rowOffsetY);
+				} else
+					epgViewVertScrollBar.setEnabled(true);
+				epgViewVertScrollBar.setValues(rowOffsetY, viewHeight, 0, contentHeight);
+				epgViewVertScrollBar.setUnitIncrement (viewHeight/4);
+				epgViewVertScrollBar.setBlockIncrement(viewHeight*9/10);
+			}
 			@Override public void componentMoved  (ComponentEvent e) {}
 		});
 		addWindowListener(new WindowListener() {
@@ -182,6 +235,7 @@ public class EPGDialog extends StandardDialog {
 					updateEPGView();
 				}
 			System.out.println("... done");
+			statusOutput.setText( "" );
 			
 			setRunning(false);
 			
@@ -224,51 +278,123 @@ public class EPGDialog extends StandardDialog {
 		
 	}
 	
+	@SuppressWarnings("unused")
 	private static class EPGViewEvent {
 	
+		private final String title;
+		private final long begin;
+		private final long end;
+		private final EPGevent event;
+
+		EPGViewEvent(EPGevent event) {
+			this.event = event;
+			title = event.title;
+			begin = event.begin_timestamp;
+			end   = event.begin_timestamp+event.duration_sec;
+		}
+
 		static Vector<EPGViewEvent> convert(Vector<EPGevent> events) {
-			// TODO Auto-generated method stub
-			return null;
+			if (events==null) return null;
+			Vector<EPGViewEvent> result = new Vector<>(events.size());
+			for (EPGevent event:events) result.add(new EPGViewEvent(event));
+			return result;
 		}
 	
 	}
 
 	private class EPGView extends Canvas {
 		private static final long serialVersionUID = 8667640106638383774L;
-
+		private static final int HEADERHEIGHT = 20;
+		private static final int ROWHEIGHT = 23;
+		private static final int STATIONWIDTH = 100;
+		
 		private final HashMap<String, Vector<EPGViewEvent>> events;
+		private int rowOffsetY;
 		
 		EPGView() {
+			rowOffsetY = 0;
 			events = new HashMap<>();
 			setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		}
 		
+		public void setRowOffsetY(int rowOffsetY) {
+			this.rowOffsetY = rowOffsetY;
+			repaint();
+		}
+		public int getRowOffsetY() {
+			return rowOffsetY;
+		}
+
+		public int getContentHeight() {
+			return stations.size()*ROWHEIGHT;
+		}
+		public int getRowViewHeight() {
+			return Math.max(0, height-HEADERHEIGHT);
+		}
+
 		synchronized void updateEvents(StationID stationID, Vector<EPGViewEvent> viewEvents) {
 			String key = stationID.toIDStr();
 			if (viewEvents==null) events.remove(key);
 			else                  events.put(key, viewEvents);
 		}
+		
+		synchronized Vector<EPGViewEvent> getEvents(StationID stationID) {
+			return events.get(stationID.toIDStr());
+		}
+		
 
 		@Override
-		protected void paintCanvas(Graphics g, int x0, int y0, int width, int height) {
-			Shape oldClip = g.getClip();
-			g.setClip(x0, y0, width, height);
+		protected void paintCanvas(Graphics g, int x0, int y0_, int width, int height) {
+			if (!(g instanceof Graphics2D)) return;
+			Graphics2D g2 = (Graphics2D) g;
+			Shape oldClip = g2.getClip();
+			Rectangle mainClip;
 			
-			int rowHeight = 30;
-			int stationWidth = 100;
+			g2.setClip(mainClip = new Rectangle(x0, y0_, width, height));
+			
+			g2.setColor(Color.BLACK);
+			g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+			
+			int fontHeight = 8; // default font size: 11  -->  fontHeight == 8
+			int stationTextOffsetX = 10;
+			int rowTextOffsetY = (ROWHEIGHT-1-fontHeight)/2+fontHeight; 
+			int headerTextOffsetY  = (HEADERHEIGHT-1-fontHeight)/2+fontHeight;
+			
+			g2.setColor(Color.GRAY);
+			g2.drawLine(x0, y0_+HEADERHEIGHT-1, x0+width-2, y0_+HEADERHEIGHT-1);
+			g2.setColor(Color.BLACK);
+			g2.drawString("Zeit-Achse", x0+10, y0_+headerTextOffsetY);
+			
+			int y0 = y0_+HEADERHEIGHT-rowOffsetY;
+			mainClip = new Rectangle(x0, y0_+HEADERHEIGHT, width, height-HEADERHEIGHT);
 			
 			for (int i=0; i<stations.size(); i++) {
 				Bouquet.SubService station = stations.get(i);
-//				renderComp.setForeground(station.isMarker() ? Color.GRAY : Color.BLACK);
-//				renderComp.setText(station.name);
-//				renderComp.setSize(stationWidth,rowHeight);
-//				renderComp.setLocation(x0, y0+rowHeight*i);
-//				renderComp.paint(g);
+				Vector<EPGViewEvent> events = station.isMarker() ? null : getEvents(station.service.stationID);
+				
+				Rectangle stationCellClip = new Rectangle(x0, y0+ROWHEIGHT*i, STATIONWIDTH, ROWHEIGHT).intersection(mainClip);
+				if (!stationCellClip.isEmpty()) {
+					g2.setClip(stationCellClip);
+					g2.setColor(Color.GRAY);
+					g2.drawLine(x0               , y0+ROWHEIGHT*(i+1)-1, x0+STATIONWIDTH-1, y0+ROWHEIGHT*(i+1)-1);
+					g2.drawLine(x0+STATIONWIDTH-1, y0+ROWHEIGHT*(i+0)  , x0+STATIONWIDTH-1, y0+ROWHEIGHT*(i+1)-1);
+				}
+				
+				Rectangle stationTextCellClip = new Rectangle(x0, y0+ROWHEIGHT*i, STATIONWIDTH-1, ROWHEIGHT-1).intersection(mainClip);
+				if (!stationTextCellClip.isEmpty()) {
+					g2.setClip(stationTextCellClip);
+					
+					if (!station.isMarker()) {
+						g2.setColor(Color.WHITE);
+						g2.fillRect(x0-10, y0+ROWHEIGHT*i-10, STATIONWIDTH+10, ROWHEIGHT+10);
+					}
+					
+					g2.setColor(events!=null || station.isMarker() ? Color.BLACK : Color.GRAY);
+					g2.drawString(station.name, x0+stationTextOffsetX, y0+ROWHEIGHT*i+rowTextOffsetY);
+				}
 			}
 			
-			// TODO
-			
-			g.setClip(oldClip);
+			g2.setClip(oldClip);
 		}
 		
 	}

926b9a3b278a295447e6bc61ba6841573847a8f4
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2024-01-19 10:39:07 +0100

    feat: added row coloring in EPGEventGenresView

--- src/net/schwarzbaer/java/tools/openwebifcontroller/epg/EPGEventGenresView.java
+++ src/net/schwarzbaer/java/tools/openwebifcontroller/epg/EPGEventGenresView.java
@@ -1,13 +1,18 @@
 package net.schwarzbaer.java.tools.openwebifcontroller.epg;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Window;
 import java.util.List;
 import java.util.Vector;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.TableCellRenderer;
 
 import net.schwarzbaer.java.lib.gui.StandardDialog;
 import net.schwarzbaer.java.lib.gui.Tables;
@@ -32,7 +37,8 @@ class EPGEventGenresView extends StandardDialog
 //		table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		tableModel.setTable(table);
 		tableModel.setColumnWidths(table);
-//		tableModel.setAllDefaultRenderers( clazz -> new Renderer());;
+		EPGEventGenresTableModelRenderer renderer = new EPGEventGenresTableModelRenderer(tableModel);
+		tableModel.setAllDefaultRenderers( clazz -> renderer);
 		
 		JScrollPane contentPane = new JScrollPane(table);
 		Dimension preferredTableSize = table.getPreferredSize();
@@ -47,20 +53,60 @@ class EPGEventGenresView extends StandardDialog
 		super.showDialog();
 	}
 	
+	private static class EPGEventGenresTableModelRenderer implements TableCellRenderer
+	{
+		private static final Color COLOR_BG_EVEN_ID = new Color(0xFFFFDF);
+		private static final Color COLOR_BG_ODD_ID  = new Color(0xE1FFDF);
+		
+		private final Tables.LabelRendererComponent rendercomp;
+		private final EPGEventGenresTableModel tableModel;
+
+		EPGEventGenresTableModelRenderer( EPGEventGenresTableModel tableModel )
+		{
+			this.tableModel = tableModel;
+			rendercomp = new Tables.LabelRendererComponent();
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int rowV, int columnV)
+		{
+			int rowM    = rowV   <0 ? -1 : table.convertRowIndexToModel(rowV); 
+			int columnM = columnV<0 ? -1 : table.convertColumnIndexToModel(columnV);
+			EPGEventGenresTableModel.Row row = tableModel.getRow(rowM);
+			EPGEventGenresTableModel.ColumnID columnID = tableModel.getColumnID(columnM);
+			
+			Supplier<Color> getCustomBackground = () -> row==null ? null : row.bgcolor;
+			
+			String valueStr = value == null ? null : value.toString();
+			rendercomp.configureAsTableCellRendererComponent(table, null, valueStr, isSelected, hasFocus, getCustomBackground, null);
+			
+			rendercomp.setHorizontalAlignment(
+					columnID==null
+						? SwingConstants.LEFT
+						: columnID.alignment
+			);
+			
+			return rendercomp;
+		}
+		
+	}
+	
 	private static class EPGEventGenresTableModel extends Tables.SimpleGetValueTableModel<EPGEventGenresTableModel.Row, EPGEventGenresTableModel.ColumnID>
 	{
 
 		enum ColumnID implements Tables.SimpleGetValueTableModel.ColumnIDTypeInt<Row>
 		{
-			isNew( "Is New ?", String.class,  55, row -> row.isNew ),
-			id   ( "ID"      , String.class,  30, row -> row.id    ),
-			name1( "Name 1"  , String.class, 200, row -> row.name1 ),
-			name2( "Name 2"  , String.class, 300, row -> row.name2 ),
+			isNew( "Is New ?", String.class, SwingConstants.CENTER,  55, row -> row.isNew ),
+			id   ( "ID"      , String.class, SwingConstants.CENTER,  30, row -> row.id    ),
+			name1( "Name 1"  , String.class, SwingConstants.LEFT  , 200, row -> row.name1 ),
+			name2( "Name 2"  , String.class, SwingConstants.LEFT  , 300, row -> row.name2 ),
 			;
 			private final SimplifiedColumnConfig cfg;
 			private final Function<Row, ?> getValueFcn;
+			private final int alignment;
 			
-			private <V> ColumnID(String name, Class<V> columnClass, int width, Function<Row, V> getValueFcn) {
+			private <V> ColumnID(String name, Class<V> columnClass, int alignment, int width, Function<Row, V> getValueFcn) {
+				this.alignment = alignment;
 				cfg = new SimplifiedColumnConfig(name, columnClass, 20, -1, width, width);
 				this.getValueFcn = getValueFcn;
 			}
@@ -78,10 +124,12 @@ class EPGEventGenresView extends StandardDialog
 				String id,
 				String name1,
 				String name2,
-				String isNew
+				String isNew,
+				Color bgcolor
 		) {
-			Row()        { this( "...", null, null, null ); }
-			Row(long id) { this( String.valueOf(id), null, null, null ); }
+
+			Row()        { this( "...", null, null, null, null ); }
+			Row(long id) { this( String.valueOf(id), null, null, null, null ); }
 			
 			static Row create(EPGEventGenre eeg)
 			{
@@ -92,7 +140,10 @@ class EPGEventGenresView extends StandardDialog
 				return new Row(
 						String.valueOf(eeg.id()),
 						name1, name2,
-						eeg.isNew() ? "New" : null
+						eeg.isNew() ? "New" : null,
+						(eeg.id() & 1) == 0
+							? EPGEventGenresTableModelRenderer.COLOR_BG_EVEN_ID
+							: EPGEventGenresTableModelRenderer.COLOR_BG_ODD_ID
 				);
 			}
 		}
